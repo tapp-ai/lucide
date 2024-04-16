4c7692818ca4054eaf939b97e70bd41bbfd26e2f
Levi Kline <37320248+levikline@users.noreply.github.com>
2024-04-16 11:49:40 -0700

    Update defaultAttributes.ts

--- packages/lucide-react/src/defaultAttributes.ts
+++ packages/lucide-react/src/defaultAttributes.ts
@@ -1,8 +1,8 @@
 export default {
   xmlns: 'http://www.w3.org/2000/svg',
-  width: 24,
-  height: 24,
-  viewBox: '0 0 24 24',
+  width: 16,
+  height: 16,
+  viewBox: '0 0 16 16',
   fill: 'none',
   stroke: 'currentColor',
   strokeWidth: 2,
